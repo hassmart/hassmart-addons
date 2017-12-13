10a396e933a35c8429bb78c9f6baf57728cfaa33
SchumyHao <bob-hjl@126.com>
2017-12-13 19:33:16 +0800

    frpc addon
    Signed-off-by: SchumyHao <bob-hjl@126.com>

---
+++ frp/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+CONFIG_PATH=/data/options.json
+
+SERVER_IP=$(jq --raw-output '.server_ip' $CONFIG_PATH)
+AUTH_TOKEN=$(jq --raw-output '.auth_token // empty' $CONFIG_PATH)
+SERVER_PORT=$(jq --raw-output '.server_port' $CONFIG_PATH)
+LOCAL_PORT=$(jq --raw-output '.local_port' $CONFIG_PATH)
+REMOTE_PORT=$(jq --raw-output '.remote_port' $CONFIG_PATH)
+
+FRP_PATH=/var/frp
+FRPC_CONF=$FRP_PATH/conf/frpc.ini
+
+if [ -f $FRPC_CONF ]; then
+  rm $FRPC_CONF
+fi
+
+echo "[common]" >> $FRPC_CONF
+echo "server_addr = $SERVER_IP" >> $FRPC_CONF
+echo "server_port = $SERVER_PORT" >> $FRPC_CONF
+echo "[hassio]" >> $FRPC_CONF
+echo "type = tcp" >> $FRPC_CONF
+echo "local_ip = 127.0.0.1" >> $FRPC_CONF
+echo "local_port = $LOCAL_PORT" >> $FRPC_CONF
+echo "remote_port = $REMOTE_PORT" >> $FRPC_CONF
+
+echo Start frp as client
+
+exec $FRP_PATH/frpc -c $FRPC_CONF < /dev/null
