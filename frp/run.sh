813bbb07a8e9fd666bfd79c73c3a9df7a62d8987
SchumyHao <bob-hjl@126.com>
2017-12-14 14:12:26 +0800

    using config proxy name
    Signed-off-by: SchumyHao <bob-hjl@126.com>

--- frp/run.sh
+++ frp/run.sh
@@ -8,6 +8,7 @@ AUTH_TOKEN=$(jq --raw-output '.auth_token // empty' $CONFIG_PATH)
 SERVER_PORT=$(jq --raw-output '.server_port' $CONFIG_PATH)
 LOCAL_PORT=$(jq --raw-output '.local_port' $CONFIG_PATH)
 REMOTE_PORT=$(jq --raw-output '.remote_port' $CONFIG_PATH)
+PROXY_NAME=$(jq --raw-output '.proxy_name // empty' $CONFIG_PATH)
 
 FRP_PATH=/var/frp
 FRPC_CONF=$FRP_PATH/conf/frpc.ini
@@ -16,10 +17,15 @@ if [ -f $FRPC_CONF ]; then
   rm $FRPC_CONF
 fi
 
+if [ ! $PROXY_NAME ]; then
+  PROXY_NAME=hassio
+  echo Using default proxy name $PROXY_NAME
+fi
+
 echo "[common]" >> $FRPC_CONF
 echo "server_addr = $SERVER_IP" >> $FRPC_CONF
 echo "server_port = $SERVER_PORT" >> $FRPC_CONF
-echo "[hassio]" >> $FRPC_CONF
+echo "[$PROXY_NAME]" >> $FRPC_CONF
 echo "type = tcp" >> $FRPC_CONF
 echo "local_ip = 127.0.0.1" >> $FRPC_CONF
 echo "local_port = $LOCAL_PORT" >> $FRPC_CONF
